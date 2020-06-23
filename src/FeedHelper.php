0ab87200c20806173d4b3daafe0f02a4a5f3b351
Dustin Quam <quamsta@gmail.com>
2020-06-23 15:06:41 -0500

    fixed a setting

--- src/FeedHelper.php
+++ src/FeedHelper.php
@@ -36,7 +36,7 @@ class FeedHelper {
             $ch = curl_init();
             curl_setopt($ch, CURLOPT_URL, $url);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
             $content = curl_exec($ch);
 
             curl_close($ch);
