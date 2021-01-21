a7980147b71aae5a632477013aa327cf101dcdf0
Dustin Quam <quamsta@gmail.com>
2021-01-21 11:12:19 -0600

    allow non-json feeds

--- src/FeedHelper.php
+++ src/FeedHelper.php
@@ -7,42 +7,62 @@ use SilverStripe\Core\Injector\Injector;
 
 class FeedHelper {
 
-    public static function getJson($url){
-        $safeUrl = FeedHelper::safe_filename($url);
-        $cache = Injector::inst()->get(CacheInterface::class . '.apiCacher');
+	public static function getJson($url) {
+		$safeUrl = FeedHelper::safe_filename($url);
+		$cache = Injector::inst()->get(CacheInterface::class . '.apiCacher');
 
-        if (!$cache->has($safeUrl)) {
-            $json = FeedHelper::fetchJson($url);
+		if (!$cache->has($safeUrl)) {
+			$json = FeedHelper::fetchData($url);
 
-            $cache->set($safeUrl, $json, 300);
-        }else{
-            //always fetch json for now
-            $json = $cache->get($safeUrl);
-             // $json = FeedHelper::fetchJson($url);
-        }
-        if($json){
-            $jsonDecoded = json_decode($json, TRUE);
-            return $jsonDecoded;
-        }
-    }
+			$cache->set($safeUrl, $json, 300);
+		} else {
 
-    public static function safe_filename($filename){
-        return preg_replace('/[^0-9a-z\.\_\-]/i','', strtolower($filename));
-    }
+			$json = $cache->get($safeUrl);
+			//always fetch data remotely(for testing):
+			// $json = FeedHelper::fetchJson($url);
+		}
+		if ($json) {
+			$jsonDecoded = json_decode($json, TRUE);
+			return $jsonDecoded;
+		}
+	}
 
-    public static function fetchJson($url){
-        if(function_exists("curl_init")){
+	public static function getUrl($url) {
+		$safeUrl = FeedHelper::safe_filename($url);
+		$cache = Injector::inst()->get(CacheInterface::class . '.apiCacher');
 
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, $url);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-            $content = curl_exec($ch);
+		if (!$cache->has($safeUrl)) {
+			$data = FeedHelper::fetchData($url);
 
-            curl_close($ch);
-            return $content;
-        } else {
-           return file_get_contents($url);
-        }
-    }
+			$cache->set($safeUrl, $data, 300);
+		} else {
+
+			$data = $cache->get($safeUrl);
+			//always fetch data remotely(for testing):
+			// $data = FeedHelper::fetchData($url);
+		}
+		if ($data) {
+			return $data;
+		}
+	}
+
+	public static function safe_filename($filename) {
+		return preg_replace('/[^0-9a-z\.\_\-]/i', '', strtolower($filename));
+	}
+
+	public static function fetchData($url) {
+		if (function_exists("curl_init")) {
+
+			$ch = curl_init();
+			curl_setopt($ch, CURLOPT_URL, $url);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+			$content = curl_exec($ch);
+
+			curl_close($ch);
+			return $content;
+		} else {
+			return file_get_contents($url);
+		}
+	}
 }
